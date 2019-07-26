fb95c0aaa98a5d027231dd1f9cfc42afc58ac959
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2019-07-26 16:14:13 +0200

    A new f function has arrived.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include "utils.hpp"
 
 using namespace std;
 
 int main()
 {
     printf("Hola mundo\n");
-    
+
+    printf("testing new function:\n");
+    printf("%d + %d = %d", a, b, f(a, b));
+
     return 0;
 }
---
+++ utils.hpp
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_UTILS_HPP
+#define INCLUDE_UTILS_HPP
+
+int f(int a, int b){
+    return a+b;
+}
+
+#endif
